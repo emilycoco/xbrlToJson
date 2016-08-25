c28e919e2c91604c6e6c0f115eebcc55bf2b16d4
emilycoco <emilymcoco@gmail.com>
2016-08-25 14:24:32 -0700

    Make request and response work.

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,33 @@ var App = React.createClass({
   },
   handleXbrlInput: function(xbrlStr) {
     console.log('parent handles input');
-    this.setState({data: xbrlStr});
+    this.setState({data: "Parsing json..."});
+    this.requestParsedInput(xbrlStr);
+  },
+  requestParsedInput: function(inputStr) {
+    console.log('sending request');
+    fetch('http://localhost:3000/parse-string', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "data": inputStr,
+    })
+  })
+    .then(function(resp) {
+      resp.json().then(function(rspJson) {
+        this.setState({data: JSON.stringify(rspJson.response)});
+      }.bind(this))
+      .catch(function(err) {
+        this.setState({data: 'Error parsing json: ' + err})
+      }.bind(this));
+    }.bind(this))
+    .catch(function(err) {
+      this.setState({data: 'Error parsing json: ' + err})
+    }.bind(this));
   }
 });
 
