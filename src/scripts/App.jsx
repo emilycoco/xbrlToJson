1e00874c5b3ce5684b55a47fff93e8dc1c004ee5
emilycoco <emilymcoco@gmail.com>
2016-09-07 18:51:27 -0700

    Delegate error handling to app, add sample doc request.

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -11,7 +11,7 @@ var App = React.createClass({
         <p>XBRL is a reporting language built on top of XML used to record financial data such as quarterly reports. The SEC accepts filings in XBRL format, so these documents are very useful in researching a company's financials. The SEC provides more robust tools for parsing XBRL, but they can be complicated to use, so this tool provides a really simple way to extract key pieces of information to a widely used data format (json) for research purposes. This tool won't parse an entire document, it just looks for the most commonly used pieces of information. Results are not guaranteed to be accurate.</p>
           <div className="container" onClick={this.resetError}>
             <h2>Xbrl goes here:</h2>
-            <XbrlForm onXbrlSubmit={this.handleXbrlInput} error={this.state.error}/>
+            <XbrlForm onXbrlSubmit={this.handleXbrlInput} error={this.state.error} loadError={this.loadError}/>
           </div>
           {this.state.data ? <XbrlDisplay output={this.state.data}/> : null}
       </div>
@@ -37,32 +37,35 @@ var App = React.createClass({
   requestParsedInput: function(inputStr) {
     console.log('sending request');
     fetch('http://localhost:3000/parse-string', {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      "data": inputStr,
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "data": inputStr,
+      })
     })
-  })
     .then(function(resp) {
       resp.json().then(function(rspJson) {
         if (rspJson.response) {
           this.setState({data: rspJson.response});
           this.scrollToOutput();
         } else {
-          this.setState({error: 'Server error: input format not recognized as XBRL or XML'})
+          this.loadError('Input format not recognized as XBRL or XML.');
         }
       }.bind(this))
       .catch(function(err) {
-        this.setState({error: 'Error parsing json: ' + err})
+        this.loadError('Error rendering response.');
       }.bind(this));
     }.bind(this))
     .catch(function(err) {
-      this.setState({error: 'Error parsing json: ' + err})
+      this.loadError('Error fetching response.');
     }.bind(this));
+  },
+  loadError: function(error) {
+    this.setState({error: error});
   }
 });
 
--- src/scripts/XbrlForm.jsx
+++ src/scripts/XbrlForm.jsx
@@ -7,7 +7,11 @@ module.exports = React.createClass({
         <textarea placeholder="Paste xbrl here"
         value={this.state.value}
         onChange={this.handleChange} />
-        <button type="submit">Submit</button>
+        <div className="button-container">
+          <button className="cta" type="submit">Submit</button>
+          <button onClick={this.loadSampleDoc}>Sample Document</button>
+          <button onClick={this.clear}>Clear</button>
+        </div>
         {this.props.error ? <span className="error">{this.props.error}</span> : null}
       </form>
     );
@@ -24,6 +28,31 @@ module.exports = React.createClass({
     e.preventDefault();
     console.log('submitting from form', this.state.value);
     this.props.onXbrlSubmit(this.state.value);
+  },
+  clear: function(e) {
+    e.preventDefault();
+    this.setState({value: ''});
+  },
+  loadSampleDoc: function(e) {
+    e.preventDefault();
+    fetch('http://localhost:3000/sample-doc', {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(function(resp) {
+      resp.json().then(function(rspText) {
+          this.setState({value: rspText.response});
+      }.bind(this))
+      .catch(function(err) {
+        this.props.loadError('Could not load sample document.');
+      }.bind(this));
+    }.bind(this))
+    .catch(function(err) {
+      this.props.loadError('Could not load sample document.');
+    }.bind(this));
   }
 });
 
