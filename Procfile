a23805b3816f6315b0499ce84115668682d5d655
emilycoco <emilymcoco@gmail.com>
2016-09-09 13:50:50 -0700

    Add Procfile and dynamic port.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node back/index.js
--- back/index.js
+++ back/index.js
@@ -34,6 +34,6 @@ app.get('/sample-doc', function (req, res) {
   })
 });
 
-app.listen(3000, function () {
-  console.log('XBRL to JSON listening on port 3000!');
+app.listen(process.env.PORT || 3000, function () {
+  console.log('XBRL to JSON listening on port %d!', this.address().port);
 });
